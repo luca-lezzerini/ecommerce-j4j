5aa34f00a247c0ada26a8d1bdd2931a7fedbc44a
Federica Dalla Valle <fede.dalla.valle@gmail.com>
2020-02-11 15:58:34 +0100

    Creato component password-dimenticata

--- client/e-commerce-client/src/app/app.module.ts
+++ client/e-commerce-client/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { RegistrazioneComponent } from './registrazione/registrazione.component';
+import { PasswordDimenticataComponent } from './password-dimenticata/password-dimenticata.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    RegistrazioneComponent
+    RegistrazioneComponent,
+    PasswordDimenticataComponent
   ],
   imports: [
     BrowserModule,
