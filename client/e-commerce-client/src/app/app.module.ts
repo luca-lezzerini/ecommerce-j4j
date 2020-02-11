2e624f0c0b3536745216023b9db7806dfb0b08d3
f-mancini <federicamancini1996@gmail.com>
2020-02-11 21:10:15 +0100

    Creato component login

--- client/e-commerce-client/src/app/app.module.ts
+++ client/e-commerce-client/src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 <<<<<<< HEAD
 import { HomePageComponent } from './home-page/home-page.component';
+import { LoginComponent } from './login/login.component';
 =======
 import { RegistrazioneComponent } from './registrazione/registrazione.component';
 import { PasswordDimenticataComponent } from './password-dimenticata/password-dimenticata.component';
@@ -14,7 +15,8 @@ import { PasswordDimenticataComponent } from './password-dimenticata/password-di
   declarations: [
     AppComponent,
 <<<<<<< HEAD
-    HomePageComponent
+    HomePageComponent,
+LoginComponent
 =======
     RegistrazioneComponent,
     PasswordDimenticataComponent
