200f1b2844e134b4ac3459e412354f60a41b05e1
Luca Lezzerini <luca@lezzerini.it>
2020-02-12 11:53:32 +0100

    Aggiunto codice iniziale server

---
+++ server/src/main/java/com/ai/ecommercej4j/Ecommercej4jApplication.java
@@ -0,0 +1,13 @@
+package com.ai.ecommercej4j;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ecommercej4jApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ecommercej4jApplication.class, args);
+	}
+
+}
---
+++ server/src/test/java/com/ai/ecommercej4j/Ecommercej4jApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ai.ecommercej4j;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ecommercej4jApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
