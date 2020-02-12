64d8a5677f3a651aa1145f1fa80535f3ebad1da5
Federica Dalla Valle <fede.dalla.valle@gmail.com>
2020-02-12 14:48:18 +0100

    Modificato SecurityController, risolti ticket #8 #12

--- server/src/main/java/com/ai/ecommercej4j/controller/SecurityController.java
+++ server/src/main/java/com/ai/ecommercej4j/controller/SecurityController.java
@@ -2,6 +2,8 @@ package com.ai.ecommercej4j.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import com.ai.ecommercej4j.model.*;
+import com.ai.ecommercej4j.service.SecurityService;
 
 @RestController
 @CrossOrigin("*")
@@ -12,24 +14,24 @@ public class SecurityController {
     
     @RequestMapping("/login")
     @ResponseBody
-    public LoginResponseDto login(@RequestBody LoginRequestDto){
+    public LoginResponseDto login(@RequestBody LoginRequestDto dto){
     return ss;
     }
     
     @RequestMapping("/password-dimenticata")
     @ResponseBody
-    public LoginResponseDto passwordDimenticata(@RequestBody LoginRequestDto){
+    public LoginResponseDto passwordDimenticata(@RequestBody LoginRequestDto dto){
     return ss;
     }
     
     @RequestMapping("/check-double-optin")
     @ResponseBody
-    public void checkDoubleOptin(@RequestBody LoginRequestDto){
+    public void checkDoubleOptin(@RequestBody LoginRequestDto dto){
     }
     
-    @RequestMapping("/registrami")
+    @RequestMapping("/registrazione")
     @ResponseBody
-    public RegistrazioneResponseDto registrami(@RequestBody RegistrazioneRequestDto){
+    public RegistrazioneResponseDto registrami(@RequestBody RegistrazioneRequestDto dto){
     return ss;
     }
 }
