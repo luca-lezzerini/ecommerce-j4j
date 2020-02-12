de0e39842b693634324f58b90249d3f1ead0c634
Alessandro Giovannangeli <gvnlsn@gmail.com>
2020-02-12 14:28:21 +0100

    Modificato registrazione.component.html e registrazione.component.ts

--- client/e-commerce-client/src/app/registrazione/registrazione.component.ts
+++ client/e-commerce-client/src/app/registrazione/registrazione.component.ts
@@ -7,9 +7,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegistrazioneComponent implements OnInit {
 
+  username: string;
+  password: string;
+  confermapassword: string;
+  email: string;
+
+
   constructor() { }
 
   ngOnInit() {
   }
 
+  registrami() {
+    // verifica se i campi siano valorizzati
+    // verifica che password e conferma password siano uguali
+    // prepara la richiesta http
+    // crea la callback
+    // se registrato è true l'utente è stato registrato e mostra messaggio di successo
+    // se registrato è false l'utente non è stato registrato e mostra messaggio di errore
+  }
+
+  annulla() {
+    // pulisci i campi username, password, conferma password e email
+    // torna alla home page
+  }
 }
