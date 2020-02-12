66cdd124055d2e2857c3958925404e495fc313e2
f-mancini <federicamancini1996@gmail.com>
2020-02-12 14:59:10 +0100

    UI login interface html #7

--- client/e-commerce-client/src/app/login/login.component.ts
+++ client/e-commerce-client/src/app/login/login.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { LoginResponseDto } from '../classi/login-response-dto';
+import { LoginRequestDto } from '../classi/login-request-dto';
+
 
 @Component({
   selector: 'app-login',
@@ -6,10 +11,49 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  username: string;
+  password: string;
+  token: string;
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
   ngOnInit() {
+  }
+
+  login(){
+    // preparo i dati da inviare al server
+    let lg: LoginRequestDto = new LoginRequestDto();
+    lg.username = this.username;
+    lg.password = this.password;
+
+    // invio i dati al server
+    let og: Observable<LoginResponseDto> =
+      this.http.
+      post<LoginResponseDto>('http://localhost:8080/login', lg);
+
+    // creo al callback
+    og.subscribe(data => {
+      this.token = data.token;
+
+    });
+  }
+
+  passwordDimenticata(){
+    // preparo i dati da inviare al server
+    let pd: LoginRequestDto = new LoginRequestDto();
+    pd.username = this.username;
+    pd.password = this.password;
+
+
+    // invio i dati al server
+    let op: Observable<LoginResponseDto> =
+    this.http.
+    post<LoginResponseDto>('http://localhost:8080/passwordDimenticata', pd);
+
+    // creo al callback
+    op.subscribe(data => {
+      this.token = data.token;
+    });
   }
 
 }
