f25949b13563688c4981e46038b3d185a1683bd3
Matteo Saccone <saccone.matteo.io@gmail.com>
2020-02-13 13:04:30 +0100

    Prova tecnica

--- client/e-commerce-client/src/app/reimposta-password/reimposta-password.component.spec.ts
+++ client/e-commerce-client/src/app/reimposta-password/reimposta-password.component.spec.ts
@@ -18,7 +18,6 @@ describe('ReimpostaPasswordComponent', () => {
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
-
   it('should create', () => {
     expect(component).toBeTruthy();
   });
