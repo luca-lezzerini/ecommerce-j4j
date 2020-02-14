b3a832238d9a87adf15327ca698194a4395e70c2
Andrea D'ottavio <andreadottavio70@gmail.com>
2020-02-14 09:36:12 +0100

    Creazione service/ColoriService.java Crezione ColoriServiceImpl.java

---
+++ server/src/main/java/com/ai/ecommercej4j/service/impl/ColoriServiceImpl.java
@@ -0,0 +1,31 @@
+
+package com.ai.ecommercej4j.service.impl;
+
+import com.ai.ecommercej4j.model.ColoriCreateDto;
+import com.ai.ecommercej4j.model.ColoriDeleteDto;
+import com.ai.ecommercej4j.model.ColoriSearchDto;
+import com.ai.ecommercej4j.model.ColoriSearchResultsDto;
+import com.ai.ecommercej4j.model.ColoriUpdateDto;
+import com.ai.ecommercej4j.service.ColoriService;
+
+
+public class ColoriServiceImpl implements ColoriService {
+
+    @Override
+    public void createColori(ColoriCreateDto dto) {
+    }
+
+    @Override
+    public ColoriSearchResultsDto searchColori(ColoriSearchDto dto) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void deleteColori(ColoriDeleteDto dto) {
+    }
+
+    @Override
+    public void updateColori(ColoriUpdateDto dto) {
+    }
+    
+}
