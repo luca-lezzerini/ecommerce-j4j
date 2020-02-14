933bb4e84b2fe4b56a9380fbb1f0258e1b9e4a3c
Nicolas Bovolenta <nichi2000@gmail.com>
2020-02-14 09:37:59 +0100

    modifiche controller/ColoriController.java

--- server/src/main/java/com/ai/ecommercej4j/controller/ColoriController.java
+++ server/src/main/java/com/ai/ecommercej4j/controller/ColoriController.java
@@ -1,11 +1,46 @@
 package com.ai.ecommercej4j.controller;
 
 
+import com.ai.ecommercej4j.model.ColoriCreateDto;
+import com.ai.ecommercej4j.model.ColoriDeleteDto;
+import com.ai.ecommercej4j.model.ColoriSearchDto;
+import com.ai.ecommercej4j.model.ColoriSearchResultsDto;
+import com.ai.ecommercej4j.model.ColoriUpdateDto;
+import com.ai.ecommercej4j.service.ColoriService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @CrossOrigin("*")
 public class ColoriController {
+    @Autowired
+    ColoriService cs;
     
+    @RequestMapping("/create-colori")
+    @ResponseBody
+    public void createColori(@RequestBody ColoriCreateDto dto) {
+         cs.createColori(dto);
+    }
+    
+    @RequestMapping("/search-colori")
+    @ResponseBody
+    public ColoriSearchResultsDto searchColori(@RequestBody ColoriSearchDto dto) {
+         return cs.searchColori(dto);
+    }
+    
+    @RequestMapping("/delete-colori")
+    @ResponseBody
+    public void deleteColori(@RequestBody ColoriDeleteDto dto) {
+         cs.deleteColori(dto);
+    }
+    
+    @RequestMapping("/update-colori")
+    @ResponseBody
+    public void updateColori(@RequestBody ColoriUpdateDto dto) {
+         cs.updateColori(dto);
+    }
 }
