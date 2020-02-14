94ace07bce28a16b10993d0895c1a87366b0063c
Nicolas Bovolenta <nichi2000@gmail.com>
2020-02-14 09:13:20 +0100

    Modifica ColoriController.java

--- server/src/main/java/com/ai/ecommercej4j/controller/ColoriController.java
+++ server/src/main/java/com/ai/ecommercej4j/controller/ColoriController.java
@@ -1,14 +1,11 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.ai.ecommercej4j.controller;
 
-/**
- *
- * @author utente
- */
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin("*")
 public class ColoriController {
     
 }
