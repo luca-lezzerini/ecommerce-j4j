4434a38b3093ffa85540447101d687fef5cc8b76
emanuele-cancelli <emanuele.cancelli1@gmail.com>
2020-02-14 15:51:47 +0100

    Creato ProdottoRepository

---
+++ server/src/main/java/com/ai/ecommercej4j/repository/ProdottoRepository.java
@@ -0,0 +1,11 @@
+package com.ai.ecommercej4j.repository;
+
+import com.ai.ecommercej4j.model.Prodotto;
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProdottoRepository extends JpaRepository<Prodotto, Long>{
+    List<Prodotto> findByCodice(String codice);
+}
