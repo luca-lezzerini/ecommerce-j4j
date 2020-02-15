7eebd4a7f7989b3e3d18824b2caef8fea72141e7
matteo <saccone.matteo.io@gmail.com>
2020-02-15 16:02:42 +0100

    Risolto ticket #50

---
+++ server/src/main/java/com/ai/ecommercej4j/repository/TagliaRepository.java
@@ -0,0 +1,9 @@
+package com.ai.ecommercej4j.repository;
+
+import com.ai.ecommercej4j.model.Taglia;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TagliaRepository extends JpaRepository<Taglia, Long>{
+    Taglia findByCodiceOrDescrizione(String filtro); 
+    
+}
