1a86fda206137f6580ecde8d02d27c4a5a35f801
Massimo Coruzzo <coruxrome@gmail.com>
2020-02-17 10:05:10 +0100

    Implementata funzione search() in home-page.component.ts Risolto ticket #57

--- client/e-commerce-client/src/app/home-page/home-page.component.ts
+++ client/e-commerce-client/src/app/home-page/home-page.component.ts
@@ -1,6 +1,15 @@
+import { Observable } from 'rxjs';
 import { Prodotto } from './../classi/prodotto';
 import { Component, OnInit } from '@angular/core';
-import {Router} from '@angular/router';
+import { Router } from '@angular/router';
+import { ProdottoSearchDto } from './../classi/prodotto-search-dto';
+import { ProdottoSearchResultsDto } from '../classi/prodotto-search-results';
+import { HttpClient } from '@angular/common/http';
+import { AreaComuneService } from '../area-comune.service';
+
+
+
+
 
 @Component({
   selector: 'app-home-page',
@@ -9,16 +18,32 @@ import {Router} from '@angular/router';
 })
 export class HomePageComponent implements OnInit {
 
-  prodotti: Prodotto [] = [];
+  prodotti: Prodotto[] = [];
   ricerca: string;
+  showResults: boolean;
 
-  constructor(private router: Router) { }
+
+  constructor(private router: Router, private acService: AreaComuneService, private http: HttpClient) { }
 
   visualizzaCarrello() {
     this.router.navigateByUrl('/view-carrello');
   }
 
   search() {
+    // Preparo i dto
+    let dto: ProdottoSearchDto = new ProdottoSearchDto();
+    dto.searchKey = this.ricerca;
+    dto.token = this.acService.token;
+
+    // Preparo richiesta http
+    let oss: Observable<ProdottoSearchResultsDto> =
+      this.http.post<ProdottoSearchResultsDto>('http://localhost:8080/search-prodotto', dto);
+
+    // Invio la richiesta
+    oss.subscribe(risposta => {
+      this.prodotti = risposta.results;
+      this.showResults = this.prodotti.length > 0;
+    });
 
   }
 
