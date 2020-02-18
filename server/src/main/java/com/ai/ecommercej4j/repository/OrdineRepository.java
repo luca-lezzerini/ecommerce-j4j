249ad695720c1445138f0d1ff677e14afea5ae01
Luca Lezzerini <luca@lezzerini.it>
2020-02-18 15:41:33 +0100

    Codice di esempio gestione associazioni in JPA: OneToMany e ManyToOne

---
+++ server/src/main/java/com/ai/ecommercej4j/repository/OrdineRepository.java
@@ -0,0 +1,9 @@
+package com.ai.ecommercej4j.repository;
+
+import com.ai.ecommercej4j.model.Ordine;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface OrdineRepository 
+        extends JpaRepository<Ordine, Long>{
+
+}
