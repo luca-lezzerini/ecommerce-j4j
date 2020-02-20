6ca76eacaedccc2e516b39ea73da0d4d24ced41a
MirkoCocchi <mirko.cocchi.26.12.2000@gmail.com>
2020-02-20 09:35:19 +0100

    Fix bug

--- server/src/main/java/com/ai/ecommercej4j/model/OrdineCreateDto.java
+++ server/src/main/java/com/ai/ecommercej4j/model/OrdineCreateDto.java
@@ -1,57 +1,5 @@
 package com.ai.ecommercej4j.model;
 
-import java.time.LocalDate;
-
 public class OrdineCreateDto extends ProdottoCreateDto {
- 
-  private Long id;
-  private LocalDate data;
-  private int numero;
-  private String stato;
-
-    public OrdineCreateDto(Long id, LocalDate data, int numero, String stato) {
-        this.id = id;
-        this.data = data;
-        this.numero = numero;
-        this.stato = stato;
-    }
-
-    public OrdineCreateDto() {
-    }
-
-   
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public LocalDate getData() {
-        return data;
-    }
-
-    public void setData(LocalDate data) {
-        this.data = data;
-    }
-
-    public int getNumero() {
-        return numero;
-    }
-
-    public void setNumero(int numero) {
-        this.numero = numero;
-    }
-
-    public String getStato() {
-        return stato;
-    }
-
-    public void setStato(String stato) {
-        this.stato = stato;
-    }
-  
-  
 
 }
