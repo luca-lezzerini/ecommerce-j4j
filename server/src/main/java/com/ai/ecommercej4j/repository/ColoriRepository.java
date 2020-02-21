23a302b4bb05d9cf68a006423296ff4bc7be46fc
Nicolas Bovolenta <nichi2000@gmail.com>
2020-02-21 12:41:44 +0100

    modifiche  repository/ColoriRepository.java

--- server/src/main/java/com/ai/ecommercej4j/repository/ColoriRepository.java
+++ server/src/main/java/com/ai/ecommercej4j/repository/ColoriRepository.java
@@ -1,6 +1,7 @@
 package com.ai.ecommercej4j.repository;
 
 import com.ai.ecommercej4j.model.Colori;
+import java.awt.print.Pageable;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -9,6 +10,7 @@ import org.springframework.stereotype.Repository;
 public interface ColoriRepository extends JpaRepository<Colori, Long>{
 
     List<Colori> findByCodiceContainingIgnoreCase(String dto);
-    List<Colori> findByDescrizioneContainingIgnoreCase(String dto);
+    
+    List<Colori> findByDescrizioneContainingIgnoreCase(String dto, Pageable pageable);
 
 }
